b46222aeaa36e2b5f711311ebfe4ac966adc0cef
Atin Singh <atin@pragra.io>
2023-07-30 13:54:04 -0400

    react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import { Link } from "./components/Link";
+import { Button } from "./components/Button";
+
+const customer = {
+  firstName: "Lavish",
+  lastName: "Makkar",
+  sayHello: function () {},
+};
+
+const names = ["Lavish", "Mohit", "John", "Jean"];
+
+const newNameGenerator = () => `${customer.firstName}  ${customer.lastName}`;
 
 function App() {
+  const [heading, setHeading] = useState("Welcome to Pragra Coffee Time");
+  const [cnt, setCount] = useState(0);
+
+  function getNamesAsList() {
+    return names.map((name) => <li>{name}</li>);
+  }
+  function updateHeading(e) {
+    setHeading(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 id="site-header" className="bold green main-heading">
+        {heading}
+      </h1>
+      <div>
+        <input
+          className="input"
+          type="text"
+          onChange={(e) => updateHeading(e)}
+          placeholder="type of your headnig"
+        ></input>
+      </div>
+      <h2>{cnt}</h2>
+      <Link href="/customer" linkText="Click for More Details"></Link>
+      <Button
+        btnText={"Click Me to See Action"}
+        action={() => alert("Hello World")}
+      />
+      <div>
+        <ul>{getNamesAsList()}</ul>
+      </div>
+      {customer.sayHello()}
     </div>
   );
 }
